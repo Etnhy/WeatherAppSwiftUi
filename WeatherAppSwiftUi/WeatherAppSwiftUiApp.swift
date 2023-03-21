408237786b84be386c7402d8c5f98903e93bb93d
Evhenii Mahlena <evhenii.mahlena.dev@gmail.com>
2023-03-21 12:41:27 +0200

    Initial Commit

---
+++ WeatherAppSwiftUi/WeatherAppSwiftUiApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppSwiftUiApp.swift
+//  WeatherAppSwiftUi
+//
+//  Created by Евгений Маглена on 21.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppSwiftUiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
